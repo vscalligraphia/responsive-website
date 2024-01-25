51a0bab6b714cb8bade311e41a24294152680bf2
Vishal Saini <vishusaini251999@gmail.com>
2024-01-25 22:45:07 +0530

    Initial Commit

---
+++ order.js
@@ -0,0 +1,20 @@
+const orders = [
+    {
+        productName : "Java Script Tutorial",
+        productNumber : "01",
+        paymentStatus : "Due",
+        status: "Pending"
+    },
+    {
+        productName : "CSS FULL COURSE",
+        productNumber : "02",
+        paymentStatus : "Cleared",
+        status : "Declined"
+    },
+    {
+        productName : "Flex Box Tutorial",
+        productNumber : "03",
+        paymentStatus : "Paid",
+        status : "Active"
+    }
+]
