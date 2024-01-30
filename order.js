0509f19437c0482944c3b5d506a1dbf0fccfe48c
Vishal Saini <vishusaini251999@gmail.com>
2024-01-31 00:08:58 +0530

    Renamed To calligraphy stuff

--- order.js
+++ order.js
@@ -1,20 +1,26 @@
 const Orders = [
     {
-        productName : "Java Script Tutorial",
+        productName : "Gothic Calligraphy Tutorial",
         productNumber : "01",
         paymentStatus : "Due",
         status: "Pending"
     },
     {
-        productName : "CSS FULL COURSE",
+        productName : "Modern Calligraphy Tutorial",
         productNumber : "02",
         paymentStatus : "Cleared",
         status : "Declined"
     },
     {
-        productName : "Flex Box Tutorial",
+        productName : "Brush Pen Calligraphy",
         productNumber : "03",
         paymentStatus : "Paid",
         status : "Active"
+    },
+    {
+        productName: "Pencil Calligraphy",
+        productNumber: "04",
+        paymentStatus: "Paid",
+        status: "Active"
     },
 ]
